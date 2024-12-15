6628a8bb56d70c79426ff1affde8b6f18b8f1ab7
poly000 <1348292515@qq.com>
2024-12-15 14:58:46 +0800

    chore: clean-up test code

--- examples/tokio-multithread.rs
+++ examples/tokio-multithread.rs
@@ -4,34 +4,21 @@ use std::{error::Error, time::Duration};
 
 use indicatif::ProgressStyle;
 use rand::random;
-use tracing::{info_span, Level};
-use tracing_core::LevelFilter;
+use tracing::info_span;
 use tracing_indicatif::{span_ext::IndicatifSpanExt, IndicatifLayer};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 type Result<T> = std::result::Result<T, Box<dyn Error + Sync + Send>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let indicatif_layer = IndicatifLayer::new()
-        .with_progress_style(
-            ProgressStyle::default_bar()
-                .template(
-                    "{spinner:.green} {msg} [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})",
-                )?
-                .progress_chars("#>-"),
-        )
-        .with_max_progress_bars(u64::MAX, None);
+    let indicatif_layer = IndicatifLayer::new().with_progress_style(
+        ProgressStyle::default_bar()
+            .template("{spinner:.green} {msg} [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")?
+            .progress_chars("#>-"),
+    );
+    tracing_subscriber::registry().with(indicatif_layer).init();
 
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_level(true)
-                .with_writer(indicatif_layer.get_stderr_writer())
-                .with_filter(LevelFilter::from_level(Level::INFO)),
-        )
-        .with(indicatif_layer)
-        .init();
     let (tx, rx) = std::sync::mpmc::channel();
 
     for _ in 0..5 {
